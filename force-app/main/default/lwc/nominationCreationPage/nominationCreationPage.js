50ed4968aea0af99bcd37bf027299e8960bb5187
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2021-01-05 21:52:45 +0200

    addign files

---
+++ force-app/main/default/lwc/nominationCreationPage/nominationCreationPage.js
@@ -0,0 +1,53 @@
+import { LightningElement , track , wire} from 'lwc';
+import getCampaignList from '@salesforce/apex/NominationCreationController.getCampaignList';
+import createNomination from '@salesforce/apex/NominationCreationController.createNomination';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+export default class NominationCreationPage extends NavigationMixin(LightningElement) {
+    campaigns;
+    nomination = '';
+    selectedCampaign;
+    @wire(getCampaignList)
+    wiredCampaigns(response){
+        this.campaigns = response.data;
+    }
+
+    handleClick(){
+        createNomination({campaignId: this.selectedCampaign, nominationName: this.nomination})
+        .then( () => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Nomination has been created',
+                    variant: 'success',
+                })
+            );
+        })
+        .catch(error =>{ 
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: error.body.message,
+                    variant: 'error',
+                })
+            );
+        });
+    }
+
+    handleTextChange(event){
+        this.nomination = event.target.value;
+    }
+
+    get campaignOptions() {
+        let listOfOptions = [];
+        this.campaigns.forEach(cam => {
+            listOfOptions.push({ label: cam.Name, value: cam.Id });
+        });
+        return listOfOptions;
+    }
+
+    handleChange(event) {
+        this.selectedCampaign = event.detail.value;
+    }
+  
+}
